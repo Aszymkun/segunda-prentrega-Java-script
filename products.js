fb8bea8384bcce7c94f73dd5a804a1ecf408faec
Abril_sz <abril_scd@hotmail.com>
2024-06-06 21:33:57 -0300

    preentrega 2

---
+++ products.js
@@ -0,0 +1,27 @@
+const productos = [
+    {
+        id:01,
+        nombre: "Carpa",
+        precio: 10500,
+        img: "https://images.unsplash.com/photo-1605620622858-ea62b0a2059c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhcnBhc3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        id:02,
+        nombre: "Bolsa de dormir",
+        precio: 50000,
+        img:"https://images.unsplash.com/photo-1558477280-1bfed08ea5db?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Ym9sc2ElMjBkZSUyMGRvcm1pcnxlbnwwfHwwfHx8MA%3D%3D"
+    
+    },
+    {
+        id:03,
+        nombre: "Mochila",
+        precio: 25000,
+        img:"https://images.unsplash.com/photo-1592388748465-8c4dca8dd703?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9jaGlsb24lMjB2aWFqZXJvfGVufDB8fDB8fHww"
+    },
+    {
+        id:04,
+        nombre: "Linterna",
+        precio: 10000,
+        img: "https://images.unsplash.com/photo-1512456489623-977da999566b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGxpbnRlcm5hfGVufDB8fDB8fHww"
+    }
+    ]
